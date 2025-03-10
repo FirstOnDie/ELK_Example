6da68f9c9ad9481a6caec91fb5b23744a3670bc0
Frody <carlos.cjec@gmail.com>
2025-03-10 13:30:06 +0100

    Working App

---
+++ src/main/java/com/Exposito/elk_example/HelloController.java
@@ -0,0 +1,18 @@
+package com.Exposito.elk_example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "Mundo") String nombre) {
+        logger.info("Se llamó a /hello con el nombre: {}", nombre);
+        return "Hola, " + nombre;
+    }
+}
---
+++ src/test/java/com/Exposito/elk_example/ElkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Exposito.elk_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
